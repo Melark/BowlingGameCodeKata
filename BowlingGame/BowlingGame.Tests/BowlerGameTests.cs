f47dc721967e69ffcad8edd5470efe48d736c394
Mark Arrow <markarrow6@gmail.com>
2021-07-17 18:56:03 +0200

    Started with BowlerGame class

---
+++ BowlingGame/BowlingGame.Tests/BowlerGameTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace BowlingGame.Tests
+{
+    public class BowlerGameTests
+    {
+        [Fact]
+        public void ScoreIsZeroWhenGameIsGreated()
+        {
+            var game = new BowlerGame();
+            var score = game.Score();
+            score.Should().Be(0);
+        }
+
+        [Fact]
+        public void RollMethodCanGetMax10Pins()
+        {
+            var game = new BowlerGame();
+            Action act = () => game.Roll(11);
+            act.Should().Throw<ArgumentException>();
+        }
+        
+        [Fact]
+        public void RollMethodCanGetMin0Pins()
+        {
+            var game = new BowlerGame();
+            Action act = () => game.Roll(-1);
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
---
+++ BowlingGame/BowlingGame/BowlerGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class BowlerGame
+    {
+        public int Score()
+        {
+            return 0;
+        }
+
+        public void Roll(int numberOfPins)
+        {
+            if (numberOfPins > 10) throw new ArgumentException("Can not roll more than 10 pins at a time");
+            if (numberOfPins < 0) throw new ArgumentException("Can not roll less than 0 pins at a time");
+        }
+    }
+}
